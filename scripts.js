aba360d848df6c8b6eb5b105f09b8cac81f14ab7
7455green <matt7green@gmail.com>
2025-12-22 16:04:42 +0000

    outsource scripts

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function resize_sim() {
+	var w = window.innerWidth  
+	var h = window.innerHeight
+	const canvas3 = document.getElementById("canvas")
+	
+	if (w < h*1.7777777){
+		if (w < 1281) {
+		  canvas3.style.width = w*0.95+"px"
+		  canvas3.style.height = "auto"
+		} else {
+		  canvas3.style.width = "1280px"
+		  canvas3.style.height = "720px"
+		}
+	} else {
+		if (h < 721) {
+		  canvas3.style.width = h*1.77*0.95+"px"
+		  canvas3.style.height = "auto"
+		} else {
+		  canvas3.style.width = "1280px"
+		  canvas3.style.height = "720px"
+		}
+	}			
+}
+
+function resize_sim_full() {
+	var w = window.innerWidth  
+	var h = window.innerHeight
+	const canvas3 = document.getElementById("canvas")
+	if (w < h*1.7777777){
+		  canvas3.style.width = w*0.95+"px"
+		  canvas3.style.height = "auto"
+	} else {
+		  canvas3.style.width = h*1.77*0.95+"px"
+		  canvas3.style.height = "auto"
+	}			
+}
