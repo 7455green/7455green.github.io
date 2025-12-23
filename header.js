9066d0c2ba7be7a25c6aa657eaca9f10a9f5620c
7455green <matt7green@gmail.com>
2025-12-23 01:10:33 +0000

    redo header

---
+++ header.js
@@ -0,0 +1,9 @@
+let appHeader = `
+    <nav>
+        <div class="header">
+		  <img src="../web_logo.png" alt="logo" />
+		  <h1>Virtual Physics</h1>
+		</div>
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
