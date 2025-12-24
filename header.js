c5c8c4516d68b96e89d2fcdd817ce419632a19f2
7455green <matt7green@gmail.com>
2025-12-24 14:20:52 +0000

    headerchange

--- header.js
+++ header.js
@@ -3,6 +3,8 @@ let appHeader = `
         <div class="header">
 		  <img src="../web_logo.png" alt="logo" />
 		  <h1>ViewPhysics</h1>
+		  <br>
+		  <p>Physics simulations for education</p>
 		</div>
     </nav>
 `;
