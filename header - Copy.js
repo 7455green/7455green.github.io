39a940aea0c8382a4a067f9240f1927ed15f1cef
7455green <matt7green@gmail.com>
2025-12-30 21:54:18 +0000

    formatdonedifferent

--- header - Copy.js
+++ header - Copy.js
@@ -1,15 +1,13 @@
-		<canvas id="myCanvas" width="1000px" height="150px"></canvas>
-		<script>
-			myCanvas.width = window.innerWidth;
-			myCanvas.height = window.innerWidth/1000*150;
-			const c = document.getElementById("myCanvas");
-			const ctx = c.getContext("2d");
-			ctx.fillStyle = "red";
-			ctx.fillRect(0, 0, 1500, 1000); 
-		</script>
-		
+let appHeader = `
+    <nav>
+        <div class="header">
+		  <img src="/web_logo_text.png" alt="logo" />
+		</div>        
 		<div class="header_buttons">
 		  <button class = "main_button" onclick="location.href='/index.html';" >Simulations</button>
 		  <button class = "main_button" onclick="location.href='/index_edexcel.html';" >Simulations (Edexcel sorted)</button>
 		  <button class = "main_button" onclick="location.href='/about.html';">About</button>
 		</div>
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
--- header.js
+++ header.js
@@ -1,8 +1,8 @@
-let appHeader = `
+let headercontent = `
     <nav>
-        <div class="header">
-		  <img src="/web_logo_text.png" alt="logo" />
-		</div>        
+		<canvas id="myCanvas" width="1000px" height="100px"></canvas>
+		
+		
 		<div class="header_buttons">
 		  <button class = "main_button" onclick="location.href='/index.html';" >Simulations</button>
 		  <button class = "main_button" onclick="location.href='/index_edexcel.html';" >Simulations (Edexcel sorted)</button>
@@ -10,4 +10,23 @@ let appHeader = `
 		</div>
     </nav>
 `;
-document.getElementById("app-header").innerHTML = appHeader;
+document.querySelector('#myHeader').insertAdjacentHTML('beforeend', headercontent);
+
+
+myCanvas.width = window.innerWidth;
+myCanvas.height = window.innerWidth/1000*100;
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue('--header_color1');
+ctx.fillRect(0, 0, myCanvas.width, myCanvas.height/3); 
+ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue('--header_color2');
+ctx.fillRect(0, myCanvas.height/3, myCanvas.width, myCanvas.height*2/3); 
+ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue('--header_color3');
+ctx.fillRect(0, myCanvas.height*2/3, myCanvas.width, myCanvas.height); 
+
+var image2 = document.createElement("img");
+
+image2.onload = function () {
+    ctx.drawImage(image2,myCanvas.width/3.5,0,myCanvas.width/2.5,myCanvas.width/10);
+}
+image2.src = '/web_logo_text.png';
