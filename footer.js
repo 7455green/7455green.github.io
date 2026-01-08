7a317e0fea2fdb9c4f2d49a865b4b4f07f136d06
7455green <matt7green@gmail.com>
2026-01-08 20:27:39 +0000

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 let footercontent = `
     <nav>
-		<p style="text-align:center;">Copyright © Matt Green</p>
-		<p style="text-align:center;">No part of this website may be reproduced without prior written permission from the copyright owner.</p>
+		<p style="text-align:center; color:#B0B0B0;">Copyright © Matt Green</p>
+		<p style="text-align:center; color:#B0B0B0;">No part of this website may be reproduced without prior written permission from the copyright owner.</p>
     </nav>
 `;
 document.querySelector('#myFooter').insertAdjacentHTML('beforeend', footercontent);
