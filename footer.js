acb3317875a4ac608fb8dbf57136f0279f0f6445
7455green <matt7green@gmail.com>
2026-01-09 11:55:09 +0000

    link test

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 let footercontent = `
     <nav>
-		<p style="text-align:center; color:#B0B0B0;">Copyright © Matt Green</p>
+		<p style="text-align:center; color:#B0B0B0;">Copyright © Matt Green. Free for educational use.</p>
 		<p style="text-align:center; color:#B0B0B0;">No part of this website may be reproduced without prior written permission from the copyright owner.</p>
     </nav>
 `;
